01973064f39fe84ebf6065ac433e468d41a76700
breadbread1984 <breadbread1984@163.com>
2024-10-16 15:38:58 +0800

    update

--- unconditional.py
+++ unconditional.py
@@ -12,14 +12,14 @@ def add_options():
   flags.DEFINE_enum('device', default = 'cuda', enum_values = {'cuda', 'cpu'}, help = 'device to use')
 
 def main(unused_argv):
-    models = {'ddpm':{'cifar10': 'google/ddpm-cifar10-32',
-                      'celeba': 'google/ddpm-celebahq-256',
-                      'cat': 'google/ddpm-cat-256',
-                      'church': 'google/ddpm-church-256',
-                      'bedroom': 'google/ddpm-bedroom-256'},
-              'ddim':{'celeba': 'fusing/ddim-celeba-hq',
-                      'church': 'fusing/ddim-lsun-church',
-                      'bedroom': 'fusing/ddim-lsun-bedroom'}}
+  models = {'ddpm':{'cifar10': 'google/ddpm-cifar10-32',
+                    'celeba': 'google/ddpm-celebahq-256',
+                    'cat': 'google/ddpm-cat-256',
+                    'church': 'google/ddpm-church-256',
+                    'bedroom': 'google/ddpm-bedroom-256'},
+            'ddim':{'celeba': 'fusing/ddim-celeba-hq',
+                    'church': 'fusing/ddim-lsun-church',
+                    'bedroom': 'fusing/ddim-lsun-bedroom'}}
   ddpm = DDPMPipeline.from_pretrained(models[FLAGS.model][FLAGS.category]).to(FLAGS.device)
   image = ddpm(num_inference_steps = 25).images[0]
   image.save(FLAGS.output)
